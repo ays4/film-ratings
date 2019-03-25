671657191e40baaf47090086c627b1303db4f659
Anton Shvab <ays@f-m.fm>
2019-03-25 21:28:42 +0300

    Add production database config, dependencies

--- src/film_ratings/handler/film.clj
+++ src/film_ratings/handler/film.clj
@@ -9,12 +9,18 @@
   (fn [_]
     [::response/ok (views.film/create-film-view)]))
 
+(defn- film-form->form
+  [film-form]
+  (as-> film-form film
+    (dissoc film "__anti-forgery-token")
+    (reduce-kv (fn [m k v] (assoc m (keyword k) v))
+               {}
+               film)
+    (update film :rating #(Integer/parseInt %))))
 
 (defmethod ig/init-key :film-ratings.handler.film/create [_ {:keys [db]}]
   (fn [{[_ film-form] :ataraxy/result :as request}]
-    (let [film (reduce-kv (fn [m k v] (assoc m (keyword k) v))
-                          {}
-                          (dissoc film-form "__anti-forgery-token"))
+    (let [film (film-form->form film-form)
           result (boundary.film/create-film db film)
           alerts (if (:id result)
                    {:message ["Film added"]}
@@ -28,5 +34,7 @@
       (if (seq films-list)
         [::response/ok (views.film/list-films-view films-list {})]
         [::response/ok (views.film/list-films-view [] {:message ["No films found."]})]))))
+
+
 
 
